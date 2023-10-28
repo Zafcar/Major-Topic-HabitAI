b12c514a084474877654d4de6963b91bcb8a33a1
Zafcar <50046161+Zafcar@users.noreply.github.com>
2023-10-28 22:39:46 +0530

    Fixed the navigation
    Also broke the code into different components.

--- homescreen.js
+++ homescreen.js
@@ -1,93 +1,141 @@
-import React from 'react';
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-const HomeScreen = () => {
-  const name = 'Amogh Iyengar'; //Name changed here later on
+import CalenderScreen from "./calenderscreen";
 
+function HomeLayout() {
   return (
     <View style={styles.container}>
-      <Text style={[styles.welcomeText, { color: 'white', marginTop: 40 }]}>Welcome {name}</Text>
-
-      <View style={styles.categoriesContainer}>
-        <View style={styles.categoryContainer}>
-          <Text style={[styles.categoryText, { color: 'white' }]}>Completed Tasks</Text>
-          <ScrollView
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={styles.buttonContainer}
-          >
-            {Array.from({ length: 10 }).map((_, index) => (
-              <View key={index} style={styles.completedButton}>
-                <Text style={[styles.buttonText, { fontWeight: 'bold', color: 'black' }]}>
-                  Task {index + 1}
-                </Text>
-              </View>
-            ))}
-          </ScrollView>
-        </View>
-
-        <View style={styles.categoryContainer}>
-          <Text style={[styles.categoryText, { color: 'white' }]}>Ongoing Tasks</Text>
-          <ScrollView style={styles.buttonContainer}>
-            {Array.from({ length: 10 }).map((_, index) => (
-              <View key={index} style={styles.ongoingButton}>
-                <Text style={[styles.buttonText, { fontWeight: 'bold', color: 'black' }]}>
-                  Task {index + 1}
-                </Text>
-              </View>
-            ))}
-          </ScrollView>
-        </View>
+      <UserDisplay />
+      <CompletedTasks />
+      <View style={{ flex: 1 }}>
+        <OngoingTasks />
       </View>
-
-      <View style={styles.buttonBar}>
-         <TouchableOpacity style={styles.bottomButton} onPress={() => navigation.navigate('HomeScreen')}>
-          <View style={styles.buttonContent}>
-            <Icon name="home" size={30} color="#5c5bfb" />
-            <Text style={[styles.bottombuttonText, {color:'white'}]}>Home</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottomButton} onPress={() => navigation.navigate('SomeScreen')}>
-          <View style={styles.buttonContent}>
-            <Icon name="cog" size={30} color="#5c5bfb" />
-            <Text style={[styles.bottombuttonText, {color:'white'}]}>Settings</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.bottomButton, styles.middleButton]} onPress={() => navigation.navigate('createtask')}>
-          <View style={styles.buttonContent}>
-            <Icon name="plus" size={20} color="black" />
-            <Text style={styles.buttonText}>Create</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottomButton} onPress={() => navigation.navigate('CalendarScreen')}>
-          <View style={styles.buttonContent}>
-            <Icon name="calendar" size={30} color="#5c5bfb" />
-            <Text style={[styles.bottombuttonText, {color:'white'}]}>Calendar</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottomButton} onPress={() => navigation.navigate('NotificationsScreen')}>
-          <View style={styles.buttonContent}>
-            <Icon name="bell" size={30} color="#5c5bfb" />
-            <Text style={[styles.bottombuttonText, {color:'white'}]}>Notis</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+      <BottonTools />
     </View>
   );
-};
+}
 
-const Stack = createStackNavigator();
-
-const App = () => {
+function UserDisplay() {
+  const name = "Amogh Iyengar"; //Name changed here later on
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        // Define your other screens here 
-        <Stack.Screen name="CreateTask" component={CreateTask} />
+    <Text style={[styles.welcomeText, { color: "white", marginTop: 40 }]}>
+      Welcome {name}
+    </Text>
+  );
+}
+
+function CompletedTasks() {
+  return (
+    <View style={styles.categoriesContainer}>
+      <Text style={[styles.categoryText, { color: "white" }]}>
+        Completed Tasks
+      </Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.buttonContainer}
+      >
+        {Array.from({ length: 10 }).map((_, index) => (
+          <CompletedTask count={index} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+function CompletedTask({ count }) {
+  return (
+    <View key={count} style={styles.completedButton}>
+      <Text style={[styles.buttonText, { fontWeight: "bold", color: "black" }]}>
+        Task {count + 1}
+      </Text>
+    </View>
+  );
+}
+
+function OngoingTasks() {
+  return (
+    <View style={styles.categoriesContainer}>
+      <Text style={[styles.categoryText, { color: "white" }]}>
+        Ongoing Tasks
+      </Text>
+      <ScrollView
+        contentContainerStyle={{}}
+        style={styles.buttonContainer}
+        snapToEnd={false}
+      >
+        {Array.from({ length: 10 }).map((_, index) => (
+          <OngoingTask count={index} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+function OngoingTask({ count }) {
+  return (
+    <View key={count} style={styles.ongoingButton}>
+      <Text style={[styles.buttonText, { fontWeight: "bold", color: "black" }]}>
+        Task {count + 1}
+      </Text>
+    </View>
+  );
+}
+
+function BottonTools() {
+  return (
+    <View style={styles.buttonBar}>
+      <UtilTool page="HomeScreen" icon="home" name="Home" />
+      <UtilTool page="SomeScreen" icon="cog" name="Settings" />
+      <TouchableOpacity style={[styles.bottomButton, styles.middleButton]}>
+        <View style={styles.buttonContent}>
+          <Icon name="plus" size={20} color="black" />
+          <Text style={styles.buttonText}>Create</Text>
+        </View>
+      </TouchableOpacity>
+      <UtilTool page="calenderScreen" icon="calendar" name="Calendar" />
+      <UtilTool page="NotificationsScreen" icon="bell" name="Notis" />
+    </View>
+  );
+}
+
+function UtilTool({ page, icon, name }) {
+  const navigation = useNavigation();
+  const onPress = () => {
+    navigation.navigate(page);
+  };
+  return (
+    <TouchableOpacity key={name} style={styles.bottomButton} onPress={onPress}>
+      <View style={[styles.buttonContent]}>
+        <Icon name={icon} size={30} color="#5c5bfb" />
+        <Text style={[styles.bottombuttonText, { color: "white" }]}>
+          {name}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const HomeScreen = () => {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator
+        initialRouteName="homeLayout"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="homeLayout" component={HomeLayout} />
+        <Stack.Screen name="calenderScreen" component={CalenderScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -96,76 +144,77 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: "black",
     padding: 16,
   },
   welcomeText: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 16,
   },
-  categoriesContainer: {
-    flex: 1,
-  },
+  categoriesContainer: {},
   categoryContainer: {
     marginBottom: 16,
+    flex: 1,
   },
   categoryText: {
     fontSize: 22,
-    fontWeight: 'bold',
-    color: 'white',
+    fontWeight: "bold",
+    color: "white",
+    marginBottom: 16,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
   completedButton: {
     width: 100,
     height: 100,
-    backgroundColor: '#5c5bfb',
+    backgroundColor: "#5c5bfb",
     margin: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   ongoingButton: {
     width: 400,
     height: 80,
-    backgroundColor: '#5c5bfb',
+    backgroundColor: "#5c5bfb",
     margin: 8,
-    justifyContent: 'center',
-    alignItems: 'left',
+    justifyContent: "center",
+    alignItems: "left",
   },
   buttonText: {
-    color: '#5c5bfb',
+    color: "#5c5bfb",
     fontSize: 18,
-    fontWeight: 'bold', 
+    fontWeight: "bold",
   },
   buttonBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#1A2421',
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#1A2421",
     padding: 10,
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
   },
   bottomButton: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   middleButton: {
-    backgroundColor: '#5c5bfb',
+    backgroundColor: "#5c5bfb",
     borderWidth: 1,
-    borderColor: '#5c5bfb',
+    borderColor: "#5c5bfb",
   },
   buttonContent: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   bottombuttonText: {
-    fontSize: 12, 
+    fontSize: 12,
   },
 });
 
